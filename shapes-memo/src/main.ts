f9d1961b23f405b00a225230d1abd7352ac44fe8
mateuszkowalke <mateusz.kowalke@gmail.com>
2022-08-19 06:23:20 +0200

    up

--- shapes-memo/src/main.ts
+++ shapes-memo/src/main.ts
@@ -1,111 +1,128 @@
 const shapes = [
-  {
-    name: "circle",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "oval",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "triangle",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "rectangle",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "square",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "diamond",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "heart",
-    color: "#ff00ff",
-    img: "",
-  },
-  {
-    name: "star",
-    color: "#ff00ff",
-    img: "",
-  },
+    {
+        name: "circle",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "oval",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "triangle",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "rectangle",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "square",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "diamond",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "heart",
+        color: "#ff00ff",
+        img: "",
+    },
+    {
+        name: "star",
+        color: "#ff00ff",
+        img: "",
+    },
 ];
 
+const game = document.getElementById("game")!;
 let isPaused = false;
-let firstPick: HTMLDivElement;
+let firstPick: HTMLDivElement | null;
+let matches = 0;
 
 const displayShapes = () => {
-  const gameHTML = [...shapes, ...shapes]
-    .sort((_) => Math.random() - 0.5)
-    .map((shape, i) => {
-      return `
-            <div class="card" onclick="clickCard(event)" data-shapename="${
-              shape.name
-            }">
+    const gameHTML = [...shapes, ...shapes]
+        .sort((_) => Math.random() - 0.5)
+        .map((shape, i) => {
+            return `
+            <div class="card" onclick="clickCard(event)" data-shapename="${shape.name
+                }">
                 <div class="front">
                     ${i + 1}
                 </div>
-                <div class="back rotated" style="background-color: ${
-                  shape.color
-                }">
+                <div class="back rotated" style="background-color: ${shape.color
+                }; background-image: url(/${shape.name}.svg)">
                     ${shape.name}
                 </div>
             </div>
             `;
-    })
-    .join("");
-  const game = document.getElementById("game")!;
-  game.innerHTML = gameHTML;
+        })
+        .join("");
+    game.innerHTML = gameHTML;
 };
 
 const clickCard = (event: Event) => {
-  const card = event.currentTarget as HTMLDivElement;
-  const shapeName = card.dataset.shapename;
-  const [front, back] = getFrontAndBackFromCard(card);
+    const card = event.currentTarget as HTMLDivElement;
+    const [front, back] = getFrontAndBackFromCard(card);
 
-  if (front?.classList.contains("rotated") || isPaused) return;
+    if (front?.classList.contains("rotated") || isPaused) return;
 
-  isPaused = true;
-  front?.classList.toggle("rotated");
-  back?.classList.toggle("rotated");
+    isPaused = true;
+    rotateElements([front, back])
 
-  if (!firstPick) {
-    firstPick = card;
-    isPaused = false;
-  } else {
-    const secondShapeName = card.dataset.shapename;
-    const firstShapeName = firstPick.dataset.shapename;
-    // if shapes not the same flip them /**
-    if (firstShapeName !== secondShapeName) {
-      const [firstFront, firstBack] = getFrontAndBackFromCard(firstPick);
-      front?.classList.toggle("rotated");
-      back?.classList.toggle("rotated");
-      firstFront?.classList.toggle("rotated");
-      firstBack?.classList.toggle("rotated");
+    if (!firstPick) {
+        firstPick = card;
+        isPaused = false;
+    } else {
+        const secondShapeName = card.dataset.shapename;
+        const firstShapeName = firstPick.dataset.shapename;
+        // if shapes not the same flip them
+        if (firstShapeName !== secondShapeName) {
+            const [firstFront, firstBack] = getFrontAndBackFromCard(firstPick);
+            firstPick = null;
+            setTimeout(() => {
+                rotateElements([firstFront, firstBack, front, back]);
+                isPaused = false;
+            },
+                1000
+            )
+        } else {
+            matches++;
+            if (matches === 8) {
+                alert("You win!");
+            }
+            firstPick = null;
+            isPaused = false;
+        }
     }
-  }
 };
 
-const getFrontAndBackFromCard = (card: HTMLDivElement) => {
-  const front = card.querySelector(".front");
-  const back = card.querySelector(".back");
-  return [front, back];
+const getFrontAndBackFromCard = (card: HTMLDivElement): HTMLDivElement[] => {
+    const front = card.querySelector(".front") as HTMLDivElement;
+    const back = card.querySelector(".back") as HTMLDivElement;
+    return [front, back];
 };
 
-const rotate
+const rotateElements = (elements: HTMLDivElement[]) => {
+    elements.forEach(element => element.classList.toggle("rotated"))
+}
 
 const resetGame = () => {
-  displayShapes();
+    game.innerHTML = '';
+    isPaused = true;
+    firstPick = null;
+    matches = 0;
+    setTimeout(() => {
+        displayShapes();
+        isPaused = false;
+    }, 200
+    )
 };
 
 const resetButton = document.getElementById("resetButton");
